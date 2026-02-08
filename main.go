c1c6e67b7eea8a650fe3fe1978b574e7f75d21b2
Lingqian Meng <binyi1114@gmail.com>
2026-02-08 02:32:29 +0100

    update the project with SQLite driver to create local database

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/glebarez/go-sqlite"
 )
 
 // Book represents our data model
@@ -18,8 +22,49 @@ var books = []Book{
 	{ID: "1", Title: "The Go Programming Language", Author: "Alan Donovan"},
 }
 
+var db *sql.DB
+
+func initDB() {
+	var err error
+	// Open the connection to the file "books.db"
+	db, err = sql.Open("sqlite", "./books.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create the table if it doesn't exist
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS books (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT,
+		author TEXT
+	);
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Fatalf("%q: %s\n", err, sqlStmt)
+	}
+}
+
 // Handler: Get all books
 func getBooks(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT id, title, author FROM books")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var books []Book
+	for rows.Next() {
+		var b Book
+		if err := rows.Scan(&b.ID, &b.Title, &b.Author); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		books = append(books, b)
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
@@ -33,7 +78,24 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 
 	var newBook Book
 	json.NewDecoder(r.Body).Decode(&newBook)
-	books = append(books, newBook)
+
+	// Insert into database
+	stmt, err := db.Prepare("INSERT INTO books(title, author) VALUES(?, ?)")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(newBook.Title, newBook.Author)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Get the ID of the newly inserted book
+	id, _ := result.LastInsertId()
+	newBook.ID = fmt.Sprintf("%d", id)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
@@ -41,6 +103,9 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	initDB()
+	defer db.Close()
+
 	// Routing
 	http.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
